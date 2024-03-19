76471a2d6d00b2b74f6ab525adfeb6e7b7d2e020
YamazakiJyunya <112788019+YamazakiJyunya@users.noreply.github.com>
2024-03-19 17:36:06 +0900

    Update cloudflare-ddns.sh

--- cloudflare-ddns.sh
+++ cloudflare-ddns.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-# Cloudflare API credentials
+# Cloudflare API
 apiKey=""
 email=""
 
-# Domain and record name
-domain="example.com"
-recordName="example"
+# Domain
+domain=""
 
 # Get zone ID
-zoneId=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$domain" -H "X-Auth-Email: $email" -H "X-Auth-Key: $apiKey" -H "Content-Type: application/json" | jq -r '.result[0].id')
+zoneId=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$domain" \
+        -H "X-Auth-Email: $email" \
+        -H "X-Auth-Key: $apiKey" \
+        -H "Content-Type: application/json" | jq -r '.result[0].id')
 
 # Get record ID
-recordId=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zoneId/dns_records?type=A&name=$recordName.$domain" -H "X-Auth-Email: $email" -H "X-Auth-Key: $apiKey" -H "Content-Type: application/json" | jq -r '.result[0].id')
+recordId=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zoneId/dns_records?type=AAAA&name=$domain" \
+          -H "X-Auth-Email: $email" \
+          -H "X-Auth-Key: $apiKey" \
+          -H "Content-Type: application/json" | jq -r '.result[0].id')
 
 # Get current IPv6 address
 ipv6Address=$(ip -6 addr show scope global | awk '/inet6/{print $2}' | grep '^2' | cut -d '/' -f 1)
@@ -20,13 +25,18 @@ ipv6Address=$(ip -6 addr show scope global | awk '/inet6/{print $2}' | grep '^2'
 # Cloudflare API URL
 apiUrl="https://api.cloudflare.com/client/v4/zones/$zoneId/dns_records/$recordId"
 
-# Headers
-headers="X-Auth-Email: $email\nX-Auth-Key: $apiKey\nContent-Type: application/json"
-
 # Data for updating record
-data="{\"type\":\"AAAA\",\"name\":\"$recordName.$domain\",\"content\":\"$ipv6Address\",\"ttl\":1,\"proxied\":false}"
+data="{\"type\":\"AAAA\",\
+\"name\":\"$domain\",\
+\"content\":\"$ipv6Address\",\
+\"ttl\":1,\
+\"proxied\":false}"
 
 # Send request to update DNS record
-response=$(curl -s -X PUT -H "$headers" -d "$data" "$apiUrl")
+response=$(curl -s -X PUT -d "$data" "$apiUrl" \
+           -H "X-Auth-Email: $email" \
+           -H "X-Auth-Key: $apiKey" \
+           -H "Content-Type: application/json"
+)
 
 echo "$response"
